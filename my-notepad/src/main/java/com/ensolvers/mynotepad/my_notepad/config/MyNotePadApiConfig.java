a4d6f3b4b17a3fec5e6d6660e777f619661ee731
User <juan.torres@konexinnovation.com.co>
2025-01-26 16:26:27 -0500

    Title: changing spring boot version and configuration. Description: changing the spring boot version and OpenApi configuration

--- my-notepad/src/main/java/com/ensolvers/mynotepad/my_notepad/config/MyNotePadApiConfig.java
+++ my-notepad/src/main/java/com/ensolvers/mynotepad/my_notepad/config/MyNotePadApiConfig.java
@@ -2,8 +2,11 @@ package com.ensolvers.mynotepad.my_notepad.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class MyNotePadApiConfig {
@@ -13,6 +16,11 @@ public class MyNotePadApiConfig {
         return new OpenAPI()
                 .info(new Info().title("My NotePad management API")
                         .description("API that provides services for the management, administration and control of the notes and categories by My NotePad")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .servers(
+                        List.of(
+                                new Server().url("/api/v1/my-notepad").description("API Gateway")
+                        )
+                );
     }
 }
